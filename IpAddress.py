567ea2186c769241fddee8502939b7ad8f740046
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2014-06-07 18:35:40 +0300

    Refactoring. Added refresh and copy to clipboard commands.

--- IpAddress.py
+++ IpAddress.py
@@ -5,62 +5,94 @@ import urllib.request
 import urllib.error
 import re
 
-class InsertIpAddressCommand(sublime_plugin.TextCommand):
-    ip = None
-    urls = {
-        'http://ipecho.net/plain': {
-            'format': 'text'
-        },
-        'http://jsonip.com': {
-            'format': 'json',
-            'path'  : 'ip'
+class IpAddress(object):
+    _instance = None
+
+    @classmethod
+    def instance(cls):
+        if not cls._instance:
+            cls._instance = cls()
+        return cls._instance
+
+    def __init__(self):
+        self.ip = None
+        self.urls = {
+            'http://ipecho.net/plain': {
+                'format': 'text'
+            },
+            'http://jsonip.com': {
+                'format': 'json',
+                'path'  : 'ip'
+            }
         }
-    }
-    parseRules = {
-        'json': lambda data, settings: json.loads(data)[settings['path']],
-        'text': lambda data, settings: data
-    }
-    ipMatcher = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+        self.parseRules = {
+            'json': lambda data, settings: json.loads(data)[settings['path']],
+            'text': lambda data, settings: data
+        }
+        self.ipMatcher = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
 
+    def get(self, refresh = False):
+        if (self.ip is None or refresh):
+            self.ip = self.fetch()
+        return self.ip
+
+    def fetch(self):
+        for server in self.urls:
+            settings = self.urls[server]
+            try:
+                self.debug("Getting IP using {0}".format(server))
+                response = urllib.request.urlopen(server).read().decode('UTF-8')
+                ip = self.parseRules[settings['format']](response, settings)
+            except urllib.error.URLError: # urlopen exception
+                self.debug("Unable to connect to {0}".format(server))
+                continue
+            except UnicodeDecodeError: # decode exception
+                self.debug("Unable to decode response from {0}".format(server))
+                continue
+            except: # parse response exception
+                self.debug("Unable to parse IP from {0}".format(server))
+                continue
+
+            if not self.ipMatcher.match(ip):
+                self.debug("{0} is not a valid IP address".format(ip))
+                continue
+
+            self.debug("{0} received using {1}".format(ip, server))
+            return ip
+        return None
+
+    def debug(self, text):
+        print('[IpAddress] ' + text)
+
+class InsertIpAddressCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        if self.ip is None:
-            for server in self.urls:
-                settings = self.urls[server]
-                try:
-                    sublime.status_message("Getting IP using {0}".format(server))
-                    response = urllib.request.urlopen(server).read().decode('UTF-8')
-                    ip = self.parseRules[settings['format']](response, settings)
-                except urllib.error.URLError: # urlopen exception
-                    sublime.status_message(
-                        "Unable to connect to {0}".format(server)
-                    )
-                    continue
-                except UnicodeDecodeError: # decode exception
-                    sublime.status_message(
-                        "Unable to decode response from {0}".format(server)
-                    )
-                    continue
-                except: # parse response exception
-                    sublime.status_message(
-                        "Unable to parse IP from {0}".format(server)
-                    )
-                    continue
+        ip = IpAddress.instance().get()
 
-                if not self.ipMatcher.match(ip):
-                    sublime.status_message(
-                        "{0} is not a valid IP address".format(ip)
-                    )
-                    continue
-
-                InsertIpAddressCommand.ip = ip
-                break;
-
-            if self.ip is None:
-                sublime.error_message("Unable to retrieve IP address")
-                return
+        if ip is None:
+            sublime.error_message("Unable to retrieve IP address")
+            return
 
         for region in self.view.sel():
             if not region.empty():
-                self.view.replace(edit, region, self.ip)
+                self.view.replace(edit, region, ip)
             else:
-                self.view.insert(edit, region.begin(), self.ip)
+                self.view.insert(edit, region.begin(), ip)
+
+class RefreshIpAddressCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        ip = IpAddress.instance().get(True)
+
+        if ip is None:
+            sublime.error_message("Unable to retrieve IP address")
+        else:
+            sublime.status_message("IP address was refreshed: {0}".format(ip))
+
+class SetClipboardIpAddressCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        ip = IpAddress.instance().get()
+
+        if ip is None:
+            sublime.error_message("Unable to retrieve IP address")
+        else:
+            sublime.set_clipboard(ip)
+            sublime.status_message("IP address {0} was copied to clipboard".format(ip))
